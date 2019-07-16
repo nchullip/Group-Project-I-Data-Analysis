c1c7f47806d6f2822f54e7c90541f937707955a9
Akshit Arora <50381572+akshitarora19@users.noreply.github.com>
2019-07-15 22:15:15 -0400

    changes made to TemperatureData

--- Uploaded by Akshit/TemperatureData.py
+++ Uploaded by Akshit/TemperatureData.py
@@ -65,9 +65,45 @@ def averagetemperature_df(country_list, temp_df, Year):
             #continue
              
     relative_temp_df["RelativeTemperature"]=pd.DataFrame(relativetemp_list)
+    
+    relative_temp_df["Year"]= relative_temp_df["Year"].astype(str)
+    
+    summary_average_temp_df=pd.pivot_table(relative_temp_df, index="Year", columns="Country", values="AverageTemperature")
+    summary_relative_temp_df=pd.pivot_table(relative_temp_df, index="Year", columns="Country", values="RelativeTemperature")
+    
+    fig = plt.figure(figsize=(15,6))
+    china, = plt.plot (summary_average_temp_df.index, summary_average_temp_df["China"], marker = "+", color = "r", linewidth = 1, label = "China")
+    india, = plt.plot (summary_average_temp_df.index, summary_average_temp_df["India"], marker = "^", color = "b", linewidth = 1, label = "India")
+    us, = plt.plot (summary_average_temp_df.index, summary_average_temp_df["United States"], marker = ">", color = "g", linewidth = 1, label = "U.S.")
+    indonesia, = plt.plot (summary_average_temp_df.index, summary_average_temp_df["Indonesia"], marker = "<", color = "m", linewidth = 1, label = "Indonesia")
+    pakistan, = plt.plot(summary_average_temp_df.index, summary_average_temp_df["Pakistan"], marker = "o", color = "y", linewidth = 1, label = "Pakistan")
+
+    plt.title("Average Temperature Change")
+    plt.xlabel("Year")
+    plt.ylabel("Average Temperature")
+    plt.xticks(rotation="vertical")
+    plt.legend(handles=[us, china, india, indonesia, pakistan])
+    plt.grid()
+    plt.show()
+    plt.savefig("AverageTemperatureChange.png")
+    
+    fig = plt.figure(figsize=(15,6))
+    china, = plt.plot (summary_relative_temp_df.index, summary_relative_temp_df["China"], marker = "+", color = "r", linewidth = 1, label = "China")
+    india, = plt.plot (summary_relative_temp_df.index, summary_relative_temp_df["India"], marker = "^", color = "b", linewidth = 1, label = "India")
+    us, = plt.plot (summary_relative_temp_df.index, summary_relative_temp_df["United States"], marker = ">", color = "g", linewidth = 1, label = "U.S.")
+    indonesia, = plt.plot (summary_relative_temp_df.index, summary_relative_temp_df["Indonesia"], marker = "<", color = "m", linewidth = 1, label = "Indonesia")
+    pakistan, = plt.plot(summary_relative_temp_df.index, summary_relative_temp_df["Pakistan"], marker = "o", color = "y", linewidth = 1, label = "Pakistan")
+
+    plt.title("Relative Temperature Change")
+    plt.xlabel("Year")
+    plt.ylabel("Relative Temperature")
+    plt.xticks(rotation="vertical")
+    plt.legend(handles=[us, china, india, indonesia, pakistan])
+    plt.grid()
+    plt.show()
+    plt.savefig("RelativeTemperatureChange.png")
+
+    
     relative_temp_df.to_csv("tempdata.csv")
     
     return relative_temp_df
-
-averagetemperature_df= averagetemperature_df(country_list=country_list, temp_df =temp_df, Year =1960)
-averagetemperature_df
